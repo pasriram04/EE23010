895b00cebae86e2b87e13891ef2949876ec53304
sriram <87891697+pasriram04@users.noreply.github.com>
2023-08-20 21:13:16 +0530

    Python code without for loops

--- NCERT_Exemplar/codes/ncert.py
+++ NCERT_Exemplar/codes/ncert.py
@@ -1,11 +1,9 @@
-import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate 10000 dice rolls
 def dice_roll():
-        dice = []
-        for i in range(0,10000):
-                dice.append(random.randint(1,6))                    
+        dice = np.array(np.random.randint(1, 7, size=10000))         
         return dice
 
 # Dice rolls generation - 2 sets for Apoorv and 1 set for Peehu                                                 
@@ -15,55 +13,51 @@ peehu = dice_roll()
 count_apoorv = [0]*37                                    
 count_peehu = [0]*37
 
-# Calculating products and squares and counting them    
-for i in range(0,10000):                                      
-        prod = apoorv_1[i]*apoorv_2[i]
-        sq = peehu[i]*peehu[i]
-        count_apoorv[prod] = count_apoorv[prod] + 1
-        count_peehu[sq] = count_peehu[sq] + 1
+# Calculating products and squares and counting them  
+prod = np.multiply(apoorv_1, apoorv_2)
+sq = np.multiply(peehu, peehu)
+count_apoorv = np.unique(prod, return_counts=True)
+count_peehu = np.unique(sq, return_counts=True)
 
-for i in range(0,37):
-        print("Probability of Apoorv getting a ",i," as a product is ", (count_apoorv[i]/10000.0))                             
-        
-for i in range(0,37):
-        print("Probability of Peehu getting a ",i," as a square is ", (count_peehu[i]/10000.0))
-
-# Initializing Arrays to store horizontal data and probability vertically, and also CDFs
-x = [0]*37
-cdf_apoorv = [0]*37
-cdf_peehu = [0]*37
-for i in range(0,37):
-        x[i] = i
-        count_apoorv[i] = count_apoorv[i]/10000.0
-        count_peehu[i] = count_peehu[i]/10000.0
-        if(i>0):
-                cdf_apoorv[i] = cdf_apoorv[i-1] + count_apoorv[i]
-                cdf_peehu[i] = cdf_peehu[i-1] + count_peehu[i]
-
+print("Apoorv: ")
+print("Values:    ", *count_apoorv[0], sep='\t')
+print("Frequency: ", *count_apoorv[1], sep='\t')
+print("Peehu: ")
+print("Values:    ", *count_peehu[0], sep='\t')
+print("Frequency: ", *count_peehu[1], sep='\t')
 
 # Plotting bar graphs using random variables
-plt.bar(x, count_apoorv)
+plt.bar(count_apoorv[0], count_apoorv[1])
 plt.xlabel('Product of dice values')
 plt.ylabel('Probability - Apoorv')
 plt.title('Probability Distribution')
 plt.show()
 
-
-plt.bar(x, count_peehu)
+plt.bar(count_peehu[0], count_peehu[1])
 plt.xlabel('Square of dice value')
 plt.ylabel('Probability - Peehu')
 plt.title('Probability Distribution')
 plt.show()
 
+# Cumulative Distribution Functions
+cdf_apoorv = np.cumsum(count_apoorv[1])
+cdf_peehu = np.cumsum(count_peehu[1])
 
-plt.bar(x, cdf_apoorv)
+print("Cumulative Distribution Functions: ")
+print("Apoorv: ")
+print("Values:    ", *count_apoorv[0], sep='\t')
+print("Frequency: ", *cdf_apoorv, sep='\t')
+print("Peehu: ")
+print("Values:    ", *count_peehu[0], sep='\t')
+print("Frequency: ", *cdf_peehu, sep='\t')
+
+plt.bar(count_apoorv[0], cdf_apoorv)
 plt.xlabel('Product of dice values')
-plt.ylabel('CDF - Apoorv ')
+plt.ylabel('CDF - Apoorv')
 plt.title('Probability Distribution')
 plt.show()
 
-
-plt.bar(x, cdf_peehu)
+plt.bar(count_peehu[0], cdf_peehu)
 plt.xlabel('Square of dice value')
 plt.ylabel('CDF - Peehu')
 plt.title('Probability Distribution')
