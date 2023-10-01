ccd876d9116ca52104647f113ed557e04a7afc9b
sriram <87891697+pasriram04@users.noreply.github.com>
2023-10-01 14:47:36 +0530

    GATE 2023 ST 32

--- 32/codes/gate.py
+++ 32/codes/gate.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
 n = 100000
-y = np.random.normal(4.0, 3.0/sqrt(n), n)
+y = np.random.normal(4.0, 3.0/np.sqrt(n), n)
 randvar = ((y - 4)**2)/n
 print("Expected Value = ", np.round(np.sum(randvar)/n,3))
