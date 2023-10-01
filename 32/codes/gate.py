58ce980ac669740fc08ed9a1666fccb29221a718
sriram <87891697+pasriram04@users.noreply.github.com>
2023-10-01 15:17:04 +0530

    GATE 2023 ST 32

--- 32/codes/gate.py
+++ 32/codes/gate.py
@@ -1,6 +1,15 @@
 import numpy as np 
 
-n = 100000
-y = np.random.normal(4.0, 3.0/np.sqrt(n), n)
+n = 10000
+
+# x is an even distribution of 1s and 7s, which is a distribution with mean 4 and standard deviation 9. 
+# 100 samples of x each of size n are generated
+x = np.random.choice([1, 7], size=(100,n))
+y = np.zeros(100)
+
+# The 100 values of y are defined, to calculate expected value
+for i in range(100):
+    y[i] = np.sum(x[i])/n
+
 randvar = ((y - 4)**2)/n
-print("Expected Value = ", np.round(np.sum(randvar)/n,3))
+print("Expected Value = ", np.round(np.sum(randvar)/100,3))
